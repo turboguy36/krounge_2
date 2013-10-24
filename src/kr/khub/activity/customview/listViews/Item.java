e3e6b2b9068d0a3fb2295a201db4a92f46c9cb22
Allen <turboguy36@gmail.com>
2013-10-24 11:28:16 +0900

    change khub

--- src/kr/khub/activity/customview/listViews/Item.java
+++
@@ -1,7 +0,0 @@
-package kr.khub.activity.customview.listViews;
-
-public interface Item {
-	
-	public boolean isSection();
-
-}
--- src/kr/khub/imageintent/AlbumStorageDirFactory.java
+++
@@ -1,7 +0,0 @@
-package kr.khub.imageintent;
-
-import java.io.File;
-
-public abstract class AlbumStorageDirFactory {
-	public abstract File getAlbumStorageDir(String albumName);
-}
